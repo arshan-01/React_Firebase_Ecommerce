4ac9103b44051e1c284fb083f0c9358aae5114af
ARSHAN NAWAZ <arshannawaz.one@gmail.com>
2022-09-03 09:44:56 +0500

    Empty Cart

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -12,6 +12,7 @@ function CartPage() {
   return (
    
 <section class="h-100 gradient-custom">
+{     cartData.length>=1?
       <div class="container py-5">
         <div class="row d-flex justify-content-center my-4">
           <div class="col-md-8">
@@ -19,9 +20,6 @@ function CartPage() {
               <div class="card-header py-3">
                 <h5 class="mb-0">Shopping Cart</h5>
               </div>
-
-              {
-          cartData.length>= 1? 
           <div class="card-body Scroll" >
              {
               cartData.map((prod)=>{
@@ -83,31 +81,7 @@ function CartPage() {
               })
              }
              
-              </div>: 
-              <div class="container-fluid  mt-100">
-				 <div class="row">
-				 
-					<div class="col-md-12">
-						<div class="card-body cart">
-								<div class="col-sm-12 empty-cart-cls text-center">
-									<img src="https://i.imgur.com/dCdflKN.png" width="130" height="130" class="img-fluid mb-4 mr-3"/>
-									<h3><strong>Your Cart is Empty</strong></h3>
-									<h4>Add something to make me happy :)</h4>
-									<Link to="/" class="btn btn-primary cart-btn-transform m-3" data-abc="true">continue shopping</Link>
-									
-								
-	
-						</div>
-				</div>
-						
-					
-					</div>
-				 
-				 </div>
-				
-				</div>
-        }
-              
+              </div> 
             </div>
             <div class="card mb-4">
               <div class="card-body">
@@ -165,7 +139,26 @@ function CartPage() {
           </div>
         </div>
       </div>
-    </section>
+       : <div class="container-fluid  mt-5" style={{background : "white"}}>
+				 <div class="row">
+				 
+					<div class="col-md-12">
+						<div class="card-body cart">
+								<div class="col-sm-12 empty-cart-cls text-center">
+									<img src="https://i.imgur.com/dCdflKN.png" width="130" height="130" class="img-fluid mb-4 mr-3"/>
+									<h3><strong>Your Cart is Empty</strong></h3>
+									<h4>Add something to make me happy :)</h4>
+									<Link to="/" class="btn btn-primary cart-btn-transform m-3" data-abc="true">continue shopping</Link>
+						</div>
+				</div>
+						
+					
+					</div>
+				 
+				 </div>
+				
+				</div>}
+      </section>
   )
 }
 
