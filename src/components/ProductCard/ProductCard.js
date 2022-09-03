5aa5b92b8eaa8718dd68ce1ba7bc7b9451ef5807
ARSHAN NAWAZ <arshannawaz.one@gmail.com>
2022-09-03 17:55:59 +0500

    Prevent add to cart , once addedd

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,12 +1,13 @@
 
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { ADD } from '../../Redux/actions/Action'
+import { ADD, REMOVE } from '../../Redux/actions/Action'
 import "./ProductCard.css"
 
 function ProductCard() {
   let products = useSelector((state)=>state.CartReducer.products);
-  // console.log(products)
+  let cartData = useSelector((state)=>state.CartReducer.cart);
+  console.log(cartData)
 
    const dispatch = useDispatch()
   
@@ -30,7 +31,12 @@ function ProductCard() {
                     <div class="product-price "><small>$96.00</small>${prod.price}</div>
                     <div class="product-links">
                       <i class="fa fa-heart"></i>
-                      <i onClick={()=> dispatch(ADD(prod))} class="fa fa-shopping-cart"></i>
+                      {
+                        cartData.some((p)=>p.id ===prod.id)? 
+                        <i onClick={()=> dispatch(REMOVE(prod.id))} class="fa fa-trash" style={{ color: "#fbb72c"}}></i>
+                         :  <i onClick={()=> dispatch(ADD(prod))} class="fa fa-shopping-cart"></i> 
+                      }
+                    
                     </div>
                 
                 </div>
